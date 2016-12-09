9df595a7289af44176ce6d51ceded3a6962c2b7f
Slava Shpitalny <slavik57@gmail.com>
2016-12-09 22:29:39 +0200

    move the index back to root

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,56 @@
 "use strict";
 
 var gulp = require("gulp"),
-  tslint = require("gulp-tslint"),
-  tsc = require("gulp-typescript"),
-  runSequence = require("run-sequence"),
-  sourcemaps = require('gulp-sourcemaps'),
-  merge = require('merge2');
+    tslint = require("gulp-tslint"),
+    tsc = require("gulp-typescript"),
+    runSequence = require("run-sequence"),
+    sourcemaps = require('gulp-sourcemaps'),
+    merge = require('merge2');
 
-gulp.task("lint", function () {
-  var config = { formatter: "verbose", emitError: (process.env.CI) ? true : false };
+gulp.task("lint", function() {
+    var config = { formatter: "verbose", emitError: (process.env.CI) ? true : false };
 
-  return gulp.src([
-    "src/**/**.ts",
-    "!src/**/*.d.ts"
-  ])
-    .pipe(tslint(config))
-    .pipe(tslint.report());
+    return gulp.src([
+        "./**/**.ts",
+        "!./**/*.d.ts",
+        "!./node_modules/**/*.ts"
+    ])
+        .pipe(tslint(config))
+        .pipe(tslint.report());
 });
 
 var tsProject = tsc.createProject("tsconfig.json");
 
-gulp.task("build", ["lint"], function () {
-  var tsResults = gulp.src([
-    "src/**/**.ts",
-    "!src/**/*.d.ts",
-    "typings/main.d.ts/"
-  ])
-    .pipe(sourcemaps.init())
-    .pipe(tsProject())
-    .on("error", function (err) {
-      process.exit(1);
-    });
+gulp.task("build", ["lint"], function() {
+    var tsResults = gulp.src([
+        "./src/**/**.ts",
+        "!./src/**/*.d.ts",
+        "index.ts",
+        "typings/main.d.ts"
+    ],
+        {
+            base: "."
+        })
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .on("error", function(err) {
+            process.exit(1);
+        });
 
-  return merge([
-    tsResults.dts.pipe(gulp.dest('src/')),
-    tsResults.js.pipe(sourcemaps.write('./')).pipe(gulp.dest('src/'))
-  ]);
+    return merge([
+        tsResults.dts.pipe(gulp.dest('.')),
+        tsResults.js.pipe(sourcemaps.write('./')).pipe(gulp.dest('./'))
+    ]);
 });
 
-gulp.task("watch", ["default"], function () {
-  gulp.watch(["src/**/*.ts", "!src/**/*.d.ts"], ["default"]);
+gulp.task("watch", ["default"], function() {
+    gulp.watch([
+        "./**/*.ts",
+        "!./**/*.d.ts",
+        "!./node_modules/**/*.ts"
+    ], ["default"]);
 });
 
-gulp.task("default", function (cb) {
-  runSequence("build", cb);
+gulp.task("default", function(cb) {
+    runSequence("build", cb);
 });
